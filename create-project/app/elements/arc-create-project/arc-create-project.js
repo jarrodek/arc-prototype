bcbf542294b3b59ee1fbd9c195f203eb3c72fd19
Paweł Psztyć <jarrodek@gmail.com>
2016-07-05 11:59:22 +0100

    Added 'autosave' mockup

--- create-project/app/elements/arc-create-project/arc-create-project.js
+++ create-project/app/elements/arc-create-project/arc-create-project.js
@@ -17,6 +17,16 @@ Polymer({
     mediaType: String
   },
 
+  observers: [
+    '_valueChanged(name)',
+    '_valueChanged(baseUrl)',
+    '_valueChanged(endpoints.*)',
+    '_valueChanged(documentation.*)',
+    '_valueChanged(securitySchemas.*)',
+    '_valueChanged(description)',
+    '_valueChanged(version)'
+  ],
+
   // Go to next tutorial page.
   nextPage: function() {
     if (this.wizardPage === 2) {
@@ -32,5 +42,24 @@ Polymer({
 
   _isCancelVisible: function(wizardPage) {
     return wizardPage !== 0;
+  },
+
+  _valueChanged: function() {
+    this.debounce('autosave', function() {
+      let cl = this.$.saveStatus.classList;
+      if (!cl.contains('visible')) {
+        cl.add('visible');
+        window.setTimeout(function() {
+          this.hideSaveStatus();
+        }.bind(this), 1500);
+      }
+    }, 1700);
+  },
+
+  hideSaveStatus: function() {
+    var cl = this.$.saveStatus.classList;
+    if (cl.contains('visible')) {
+      cl.remove('visible');
+    }
   }
 });
